25dfe9f61ec93cc8823e175089d24a51b9ced642
Luis Mendoza <lmendoza.mg@gmail.com>
2021-02-11 09:40:42 +0100

    first version of patlabor-api with characters and labors information

---
+++ app/Http/Resources/v1/CharacterResource.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Resources\v1;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CharacterResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'name' => $this->name,
+            'age' => $this->age
+        ];
+    }
+}
